4c8268b74aed8c7134fb6e30c82cc7978ce674f7
Andy Yu <andyyu2004@gmail.com>
2022-11-16 23:04:22 +1300

    some more classic films

--- ui/src/api/index.ts
+++ ui/src/api/index.ts
@@ -16,50 +16,78 @@ export function api(): API {
 
 function makeMockAPI(): API {
   async function movies(): Promise<Movie[]> {
-    return [
+    const movies: { title: string; posterUrl: string }[] = [
       {
-        id: 2,
         title: "Mission Impossible",
         posterUrl:
           "https://pictionarai.s3.amazonaws.com/img/20221115230900_00000_mission_impossible_When_an_IMF_mission_ends_badly_the_world_is_faced_with_dire_consequences_As_Ethan_Hunt_takes_it_upon_himself_to_fulfill_his_original_briefing.png",
       },
       {
-        id: 3,
         title: "Titanic",
         posterUrl:
           "https://pictionarai.s3.amazonaws.com/img/20221116002140_00000_titanic_van_gogh_style.png",
       },
       {
-        id: 4,
         title: "The Lion King",
         posterUrl:
           "https://pictionarai.s3.amazonaws.com/img/20221116002705_00000_the_lion_king_baroque_style.png",
       },
       {
-        id: 5,
         title: "Spider Man",
         posterUrl:
           "https://pictionarai.s3.amazonaws.com/img/20221116002705_00001_spider_man_classicism.png",
       },
       {
-        id: 6,
         title: "Iron Man",
         posterUrl:
           "https://pictionarai.s3.amazonaws.com/img/20221116002931_00000_man_in_iron_suit_expressionism.png",
       },
       {
-        id: 7,
         title: "Mary Poppins",
         posterUrl:
           "https://pictionarai.s3.amazonaws.com/img/20221116022600_00000_lady_with_an_umbrella_in_space_trending_on_artstation.png",
       },
       {
-        id: 8,
         title: "Black Adam",
         posterUrl:
           "https://pictionarai.s3.amazonaws.com/img/blackadamtest.jpeg",
       },
+      {
+        title: "Terrifier",
+        posterUrl:
+          "https://pictionarai.s3.amazonaws.com/img/terrifiertest.jpeg",
+      },
+      {
+        title: "The Conjuring",
+        posterUrl: "https://pictionarai.s3.amazonaws.com/img/terminator.jpeg",
+      },
+      {
+        title: "Lord of the Rings",
+        posterUrl: "https://pictionarai.s3.amazonaws.com/img/lotr.jpeg",
+      },
+      {
+        title: "Snow White",
+        posterUrl: "https://pictionarai.s3.amazonaws.com/img/snowwhite",
+      },
+      {
+        title: "The Godfather",
+        posterUrl: "https://pictionarai.s3.amazonaws.com/img/godfather",
+      },
+      {
+        title: "Jurrasic Park",
+        posterUrl: "https://pictionarai.s3.amazonaws.com/img/jpark.jpeg",
+      },
+      {
+        title: "Indiana Jones",
+        posterUrl: "https://pictionarai.s3.amazonaws.com/img/indianajones.jpeg",
+      },
+      {
+        title: "Harry Potter",
+        posterUrl: "https://pictionarai.s3.amazonaws.com/img/hpandstone.jpeg",
+      },
     ];
+
+    return movies.map((movie, id) => ({ id, ...movie }));
   }
 
   return { movies };
