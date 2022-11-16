9a8b7f5523a6838ffa8f797e94a1f853f416f3a6
Joshua <42632439+joshiefu@users.noreply.github.com>
2022-11-17 11:30:55 +1300

    Add more movie posters

--- ui/src/api/index.ts
+++ ui/src/api/index.ts
@@ -113,6 +113,116 @@ function makeMockAPI(): API {
         title: "Forrest Gump",
         posterUrl: "https://pictionarai.s3.amazonaws.com/img/gump.jpeg",
       },
+      {
+        title: "Curious George",
+        posterUrl:
+          "https://pictionarai.s3.amazonaws.com/img/20221116023343_00000_a_painting_of_a_monkey_in_a_business_suit.png",
+      },
+      {
+        title: "Goodfellas",
+        posterUrl:
+          "https://pictionarai.s3.amazonaws.com/img/20221116213906_00003_goodfellas.png",
+      },
+      {
+        title: "Aquaman",
+        posterUrl:
+          "https://pictionarai.s3.amazonaws.com/img/20221116213906_00000_aquaman.png",
+      },
+      {
+        title: "Star Wars: A New Hope",
+        posterUrl:
+          "https://pictionarai.s3.amazonaws.com/img/20221116215811_00000_star_wars_a_new_hope_impresssionism.png",
+      },
+      {
+        title: "La La Land",
+        posterUrl:
+          "https://pictionarai.s3.amazonaws.com/img/20221116215811_00002_la_la_land_abstractmovie_spaceballs.png",
+      },
+      {
+        title: "Avengers: Age of Ultron",
+        posterUrl:
+          "https://pictionarai.s3.amazonaws.com/img/20221116215811_00001_avengers_age_of_ultron.png",
+      },
+      {
+        title: "V For Vendetta",
+        posterUrl:
+          "https://pictionarai.s3.amazonaws.com/img/20221116215811_00003_v_for_vendetta_movie.png",
+      },
+      {
+        title: "Interstellar",
+        posterUrl:
+          "https://pictionarai.s3.amazonaws.com/img/20221116215811_00004_interstellar_the_movie.png",
+      },
+      {
+        title: "Inception",
+        posterUrl:
+          "https://pictionarai.s3.amazonaws.com/img/20221116215811_00005_inception_the_movie.png",
+      },
+      {
+        title: "Resident Evil",
+        posterUrl:
+          "https://pictionarai.s3.amazonaws.com/img/20221116215811_00006_resident_evil_movie.png",
+      },
+      {
+        title: "High School Musical",
+        posterUrl:
+          "https://pictionarai.s3.amazonaws.com/img/20221116215811_00007_high_school_musical_the_movie.png",
+      },
+      {
+        title: "Charlie and the Chocolate Factory",
+        posterUrl:
+          "https://pictionarai.s3.amazonaws.com/img/20221116215811_00008_charlie_and_the_chocolate_factory_the_movie.png",
+      },
+      {
+        title: "Cars",
+        posterUrl:
+          "https://pictionarai.s3.amazonaws.com/img/20221116221632_00000_Cars__disney_movie.png",
+      },
+      {
+        title: "Sleeping Beauty",
+        posterUrl:
+          "https://pictionarai.s3.amazonaws.com/img/20221116221632_00001_encanto_disney_movie.png",
+      },
+      {
+        title: "Toy Story",
+        posterUrl:
+          "https://pictionarai.s3.amazonaws.com/img/20221116221632_00002_toy_story_disney_movie.png",
+      },
+      {
+        title: "Transformers",
+        posterUrl:
+          "https://pictionarai.s3.amazonaws.com/img/20221116221632_00003_transformers_movie.png",
+      },
+      {
+        title: "Die Hard with A Vengence",
+        posterUrl:
+          "https://pictionarai.s3.amazonaws.com/img/20221116221632_00004_die_hard_movie.png",
+      },
+      {
+        title: "Skyfall",
+        posterUrl:
+          "https://pictionarai.s3.amazonaws.com/img/20221116221632_00005__skyfall_movie.png",
+      },
+      {
+        title: "Spaceballs",
+        posterUrl:
+          "https://pictionarai.s3.amazonaws.com/img/20221116221632_00006_spaceballs_movie.png",
+      },
+      {
+        title: "Moana",
+        posterUrl:
+          "https://pictionarai.s3.amazonaws.com/img/20221116221923_00000_moana_movie.png",
+      },
+      {
+        title: "Tangled",
+        posterUrl:
+          "https://pictionarai.s3.amazonaws.com/img/20221116221923_00002_tangled_movie.png",
+      },
+      {
+        title: "The Barbie Movie",
+        posterUrl:
+          "https://pictionarai.s3.amazonaws.com/img/20221116221923_00003_barbie_movie.png",
+      },
     ];
 
     return movies.map((movie, id) => ({ id, ...movie }));
