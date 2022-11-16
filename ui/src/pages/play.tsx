67f089b668f5fe9e7b7df260d6e9e794fb7f81b8
Andy Yu <andyyu2004@gmail.com>
2022-11-16 16:09:32 +1300

    target movie

--- ui/src/pages/play.tsx
+++ ui/src/pages/play.tsx
@@ -1,8 +1,34 @@
 import { SearchDropdown } from "../components/search-dropdown";
 
 import { useQuery } from "@tanstack/react-query";
-import { api, Movie } from "../api";
+import { api, Movie, TmdbId } from "../api";
 import { useState } from "react";
+
+export type RoundProps = {
+  movies: Movie[];
+  targetMovieIdx: number;
+};
+
+export const Round = ({ movies, targetMovieIdx }: RoundProps) => {
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+  const targetMovie = movies[targetMovieIdx];
+  console.log(targetMovie);
+
+  return (
+    <div>
+      <div className="flex flex-col gap-10">
+        <div className="w-[512px] h-[512px] bg-[url('https://upload.wikimedia.org/wikipedia/commons/2/25/Blisk-logo-512-512-background-transparent.png')]"></div>
+        {<img src={targetMovie.posterUrl} />}
+        {<div>{targetMovie.posterUrl} </div>}
+        <SearchDropdown
+          movies={movies}
+          selectedMovie={selectedMovie}
+          setSelectedMovie={setSelectedMovie}
+        />
+      </div>
+    </div>
+  );
+};
 
 export const Play = () => {
   const {
@@ -14,26 +40,12 @@ export const Play = () => {
     queryFn: api().movies,
   });
 
-  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
-
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>An error has occurred</div>;
   if (!movies) {
     throw new Error();
   }
 
-  return (
-    <div>
-      <div className="flex flex-col gap-10">
-        <div className="w-[512px] h-[512px] bg-[url('https://upload.wikimedia.org/wikipedia/commons/2/25/Blisk-logo-512-512-background-transparent.png')]"></div>
-        {selectedMovie && <img src={selectedMovie.posterUrl} />}
-        {selectedMovie && <div>{selectedMovie.posterUrl} </div>}
-        <SearchDropdown
-          movies={movies}
-          selectedMovie={selectedMovie}
-          setSelectedMovie={setSelectedMovie}
-        />
-      </div>
-    </div>
-  );
+  const targetMovieId = Math.floor(Math.random() * movies.length);
+  return <Round targetMovieIdx={targetMovieId} movies={movies} />;
 };
