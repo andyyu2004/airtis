755f3bb3f7ad5bf05c7e6730ec082257347d070c
chaoch321 <chao.chen@movio.co>
2022-11-16 13:28:31 +1300

    Push image and input dropdown

--- ui/src/pages/home.tsx
+++ ui/src/pages/home.tsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <Link to="/play" className=" text-3xl font-bold">
-      play
+    <Link to="/play" className="text-5xl font-bold">
+      play the game
     </Link>
   );
 };
