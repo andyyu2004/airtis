ad35782b8205fc080f50f1642d914202e1a3b340
Andy Yu <andyyu2004@gmail.com>
2022-11-16 21:24:28 +1300

    just say play

--- ui/src/pages/home.tsx
+++ ui/src/pages/home.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export const Home = () => {
   return (
     <Link to="/play" className="text-5xl font-bold">
-      play the game
+      Play
     </Link>
   );
 };
