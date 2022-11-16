65d61c31a760aa0857e4c2d5b7b973d95364d2c3
Andy Yu <andyyu2004@gmail.com>
2022-11-16 13:38:59 +1300

    select movies

--- ui/src/components/search-dropdown.tsx
+++ ui/src/components/search-dropdown.tsx
@@ -1,35 +1,32 @@
 import { useState } from "react";
 import { Combobox } from "@headlessui/react";
+import { Movie } from "../api";
 
-const people = [
-  "Durward Reynolds",
-  "Kenton Towne",
-  "Therese Wunsch",
-  "Benedict Kessler",
-  "Katelyn Rohan",
-];
+type Props = {
+  movies: Movie[];
+};
 
-export function SearchDropdown() {
-  const [selectedPerson, setSelectedPerson] = useState(people[0]);
+export function SearchDropdown({ movies }: Props) {
+  const [selectedMovie, setSelectedMovie] = useState();
   const [query, setQuery] = useState("");
 
-  const filteredPeople =
+  const filteredMovies =
     query === ""
-      ? people
-      : people.filter((person) => {
-          return person.toLowerCase().includes(query.toLowerCase());
+      ? []
+      : movies.filter((movie) => {
+          return movie.title.toLowerCase().includes(query.toLowerCase());
         });
 
   return (
-    <Combobox value={selectedPerson} onChange={setSelectedPerson}>
+    <Combobox value={selectedMovie} onChange={setSelectedMovie}>
       <Combobox.Input
         className=" py-2 px-3 text-sm leading-5 focus:ring-0 h-10 w-full rounded border border-gray-300"
         onChange={(event) => setQuery(event.target.value)}
       />
       <Combobox.Options>
-        {filteredPeople.map((person) => (
-          <Combobox.Option key={person} value={person}>
-            {person}
+        {filteredMovies.map((movie) => (
+          <Combobox.Option key={movie.tmdbId} value={movie.title}>
+            {movie.title}
           </Combobox.Option>
         ))}
       </Combobox.Options>
--- ui/src/pages/play.tsx
+++ ui/src/pages/play.tsx
@@ -23,7 +23,7 @@ export const Play = () => {
     <div>
       <div className="flex flex-col gap-10">
         <div className="w-[512px] h-[512px] bg-[url('https://upload.wikimedia.org/wikipedia/commons/2/25/Blisk-logo-512-512-background-transparent.png')]"></div>
-        <SearchDropdown />
+        <SearchDropdown movies={movies} />
       </div>
     </div>
   );
